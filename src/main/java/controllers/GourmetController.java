5dcb940a5aeed85c71126ecd971799dad13c7185
samnishita <saminator21@gmail.com>
2020-06-14 16:33:03 -0700

    added lambda expression

--- src/main/java/controllers/GourmetController.java
+++ src/main/java/controllers/GourmetController.java
@@ -11,11 +11,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Tab;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import mainapp.Driver;
 
 /**
  *
